feafcdb49174ee0862e9cecf019a751176f384b3
dori-dev <mr.dori.dev@gmail.com>
2022-07-25 08:52:19 +0430

    use django generic views instead fucntion views

--- polls/views.py
+++ polls/views.py
@@ -1,24 +1,22 @@
-from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse
+from django.views import generic
 from .models import Choice, Question
 
 
-def index(request):
-    latest_questions = Question.objects.order_by(
-        '-published_date')[:5]
-    context = {
-        'latest_questions': latest_questions
-    }
-    return render(request, "polls/index.html", context)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_questions'
+
+    def get_queryset(self):
+        """Return the last five published questions.
+        """
+        return Question.objects.order_by('-published_date')[:5]
 
 
-def detail(request, question_id: int):
-    question = get_object_or_404(Question, pk=question_id)
-    context = {
-        'question': question
-    }
-    return render(request, 'polls/detail.html', context)
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
 
 
 def vote(request, question_id: int):
@@ -27,6 +25,9 @@ def vote(request, question_id: int):
         selected_choice: Choice = question.choice_set.get(
             pk=request.POST['choice']
         )
+        print(question_id)
+        for i in range(50_000_000):
+            pass
     except (KeyError, Choice.DoesNotExist):
         # Redisplay the question voting form.
         context = {
@@ -37,12 +38,9 @@ def vote(request, question_id: int):
     else:
         selected_choice.votes += 1
         selected_choice.save()
-        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+        return redirect(reverse('polls:results', args=(question.id,)))
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    context = {
-        'question': question,
-    }
-    return render(request, 'polls/results.html', context)
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
