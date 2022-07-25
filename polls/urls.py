ef0f4506596c5a03cdfa8f494c5e1dc13535692c
dori-dev <mr.dori.dev@gmail.com>
2022-07-25 07:55:23 +0430

    add voting form

--- polls/urls.py
+++ polls/urls.py
@@ -5,4 +5,5 @@ app_name = 'polls'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/vote/', views.vote, name='vote')
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,7 @@
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
-from .models import Question
+from django.urls import reverse
+from .models import Choice, Question
 
 
 def index(request):
@@ -11,9 +13,28 @@ def index(request):
     return render(request, "polls/index.html", context)
 
 
-def detail(request, question_id):
+def detail(request, question_id: int):
     question = get_object_or_404(Question, pk=question_id)
     context = {
         'question': question
     }
     return render(request, 'polls/detail.html', context)
+
+
+def vote(request, question_id: int):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice: Choice = question.choice_set.get(
+            pk=request.POST['choice']
+        )
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        context = {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        }
+        return render(request, 'polls/detail.html', context)
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
