a70276166ee35460b3aaff57ccb2a68b1c3f408c
dori-dev <mr.dori.dev@gmail.com>
2022-07-27 14:02:58 +0430

    change url roating

--- config/urls.py
+++ config/urls.py
@@ -4,6 +4,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
+    path('', include('polls.urls')),
 ]
--- polls/urls.py
+++ polls/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = 'polls'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
-    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('poll/<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('poll/<int:pk>/results/', views.ResultsView.as_view(), name='results'),
 ]
