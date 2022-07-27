1f4e1bdcd8cfe67601a1b45bb224e7f49bb6c692
dori-dev <mr.dori.dev@gmail.com>
2022-07-27 09:34:23 +0430

    customize the admin page

--- polls/admin.py
+++ polls/admin.py
@@ -1,7 +1,65 @@
 # standard libraries
+import datetime
+# standard libraries
 from django.contrib import admin
+from django.utils import timezone
 # local libraries
 from .models import Question, Choice
 
-admin.site.register(Question)
-admin.site.register(Choice)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class RecentFilter(admin.SimpleListFilter):
+    title = 'recently published'
+    parameter_name = 'published'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('recently', 'Published Recently'),
+            ('not_recently', 'Dont\'t Published Recently'),
+            ('future', 'Published In The Future')
+        )
+
+    def queryset(self, request, queryset):
+        one_day_ago = timezone.now() - datetime.timedelta(days=1)
+        if self.value() == 'recently':
+            return queryset.filter(
+                published_date__lte=timezone.now(),
+                published_date__gte=one_day_ago
+            )
+        if self.value() == 'not_recently':
+            return queryset.filter(
+                published_date__lt=one_day_ago
+            )
+        if self.value() == 'future':
+            return queryset.filter(
+                published_date__gt=timezone.now()
+            )
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (
+            None, {
+                'fields': ['question_text']
+            }
+        ),
+        (
+            'Date Information', {
+                'fields': ['published_date']
+            }
+        )
+    ]
+    inlines = [ChoiceInline]
+    list_display = (
+        'question_text', 'id',
+        'published_date', 'was_published_recently'
+    )
+    list_filter = ['published_date', RecentFilter]
+    search_fields = ['question_text']
+
+
+admin.site.register(Question, QuestionAdmin)
--- polls/models.py
+++ polls/models.py
@@ -2,6 +2,7 @@
 import datetime
 # third party libraries
 from django.db import models
+from django.contrib import admin
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 
@@ -12,12 +13,20 @@ class Question(models.Model):
         _('date published')
     )
 
+    @admin.display(
+        boolean=True,
+        ordering='-published_date',
+        description='Published recently?'
+    )
     def was_published_recently(self):
         one_day_ago = timezone.now() - datetime.timedelta(days=1)
         return one_day_ago <= self.published_date <= timezone.now()
 
     def __str__(self):
         return self.question_text
+
+    class Meta:
+        ordering = ('-published_date',)
 
 
 class Choice(models.Model):
